9ae3ff6b365d9ca94b1b0364162d5af4d5e5fad5
Andrey Gareev <machete384@icloud.com>
2020-02-29 17:08:38 +0300

    Initial Commit

---
+++ Motion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motion
+//
+//  Created by Андрей Гареев on 29.02.2020.
+//  Copyright © 2020 Motion inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
